42d785e0da0c42014011b4d342c7fce1fad0a126
kimanx <kimanx@inbox.lv>
2014-04-10 00:05:58 +0300

    tagad var spiest >1 ierakstu

--- swapping.js
+++ swapping.js
@@ -1,19 +1,14 @@
-var WIDTH;
-var HEIGHT;
-var ctx;
 var memoryBox;
 var swapBox;
 var processes = new Array();
 var processesWait = new Array();
 var memoryWeight;
 var waitingWeight;
-var totalProcesses;
 var frames;
 var intervalId;
 var intervalId2;
-var prepared = false;
-
-
+var lasBlockMemory;
+var labelTotalProcessWaiting;
 
 function ProcessMemory(box, weight){
 
@@ -22,14 +17,12 @@ function ProcessMemory(box, weight){
 
 }
 
-
-
 function initSwap(){
 
     memoryBox = new Common.Box(50, 150, 100, 210, ["RAM"], '#FFFFFF');
     swapBox = new Common.Box(450, 150, 150, 200, ["Swap file"], '#FFFFFF');
     reDrawGrid();
-    totalProcesses = memoryWeight = waitingWeight = parseInt(0);
+    memoryWeight = waitingWeight = parseInt(0);
     frames = 0;
 }
 
@@ -37,21 +30,22 @@ function drawMemoryBlock(blockWeight){
     var randColor = '#'+('00000'+(Math.random()*(1<<24)|0).toString(16)).slice(-6);
     var newBox;
     var bWeight = parseInt(blockWeight);
-    totalProcesses++;
+    lasBlockMemory = bWeight;
     if(memoryWeight + bWeight <= 200){
         newBox = new Common.Box(65 ,155+memoryWeight , 70,blockWeight,"", randColor);
         newBox.Draw();
         processes.push(new ProcessMemory(newBox, blockWeight));
         memoryWeight += parseInt(blockWeight);
     } else {
+        var oldProcess = new Array();
         while(memoryWeight + bWeight > 200){
-            prepared = false;
-            var oldProcess = processes.shift();
-            memoryWeight -= oldProcess.weight;
-                intervalId = setInterval(function () {
-                    animateDrop(oldProcess)
-                }, 15);
+            var temp = processes.shift();
+            oldProcess.push(temp);
+            memoryWeight -= temp.weight;
         }
+        intervalId = setInterval(function () {
+            animateDrop(oldProcess)
+        }, 15);
     }
 }
 
@@ -59,14 +53,21 @@ function reDrawGrid(){
     Common.ClearCanvas();
     memoryBox.Draw();
     swapBox.Draw();
+    var string = processesWait.length.toString();
+    labelTotalProcessWaiting = new Common.DrawText([string] ,490,190,10, 'black');
 }
-function animateDrop(process){
+function animateDrop(droplings){
     frames++;
     reDrawGrid();
-    process.box.x ++;
-    var newBox = new Common.Box(process.box.x + frames,process.box.y,70, process.weight,"",process.box.color);
-    newBox.Draw();
-    var temp = new Array()
+    var temp = new Array();
+    var tempWeight = parseInt(0);
+    droplings.forEach(function(process){
+        process.box.x ++;
+        var newBox = new Common.Box(process.box.x + frames,process.box.y,70, process.weight,"",process.box.color);
+        newBox.Draw();
+        tempWeight+= parseInt(process.weight);
+    });
+
     processes.forEach(function(entry){
         entry.box.Draw();
     });
@@ -74,9 +75,11 @@ function animateDrop(process){
     if(frames == 200){
         frames = 0;
         clearInterval(intervalId);
-        intervalId2 = setInterval(function(){moveLeftovers(process.weight)},15);
-        processesWait.push(new ProcessMemory(newBox, process.weight))
-        prepared = true;
+        intervalId2 = setInterval(function(){moveLeftovers(tempWeight)},15);
+        droplings.forEach(function(process){
+            processesWait.push(new ProcessMemory(process.box, process.weight));
+        });
+
     }
 }
 
@@ -95,6 +98,7 @@ function moveLeftovers(limit){
         clearInterval(intervalId2);
         frames=0;
         processes = temp;
+        drawMemoryBlock(parseInt(lasBlockMemory));
     }
 }
 
